7c75059e1b97c7555629e6be492e0d5144afa6ce
GOPAL RAMAKANT SONI <gopalsoni5021@gmail.com>
2026-03-02 16:30:56 +0530

    Fix correct Vercel Cors again

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 // Middleware
 // app.use(cors());
 
-app.use(cors());
+app.use(cors({
+  origin:"https://smart-expense-tracker-fawn.vercel.app",
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
