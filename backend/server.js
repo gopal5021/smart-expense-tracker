5d7021f9c01d1b4adb5cfac255294c6bb1bae541
GOPAL RAMAKANT SONI <gopalsoni5021@gmail.com>
2026-03-02 16:14:16 +0530

    Simplifies CORS

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ const app = express();
 // Middleware
 // app.use(cors());
 
-app.use(cors({
-  origin:"https://smart-expense-tracker-fawn.vercel.app",
-  credentials:true
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
